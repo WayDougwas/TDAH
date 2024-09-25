0cb84f55635fd716ca7b0d7dedf41778699dafe9
WayDougwas <89326159+WayDougwas@users.noreply.github.com>
2024-09-25 01:21:45 -0300

    Changed database class name from User to App

--- app/src/main/java/com/example/tdah/data/Database.kt
+++ app/src/main/java/com/example/tdah/data/Database.kt
@@ -6,21 +6,21 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [User::class, Response::class], version = 1)
-abstract class UserDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
     abstract fun responseDao(): ResponseDao
 
     companion object {
         @Volatile
-        private var INSTANCE: UserDatabase? = null
+        private var INSTANCE: AppDatabase? = null
 
         // Função para obter uma instância do banco de dados
-        fun getDatabase(context: Context): UserDatabase {
+        fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    UserDatabase::class.java,
+                    AppDatabase::class.java,
                     "app_database"
                 ).build()
                 INSTANCE = instance
